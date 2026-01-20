ea5a184b20666bf62536efd2624c4180ff27ebb3
Utkarsh <Utkarsh.Bhadauria@coforge.com>
2026-01-20 14:38:31 +0530

    ok

---
+++ src/main/java/automation/jenkinpractise/App.java
@@ -0,0 +1,10 @@
+package automation.jenkinpractise;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/automation/jenkinpractise/LoginTest.java
@@ -0,0 +1,12 @@
+package automation.jenkinpractise;
+import org.testng.annotations.Test;
+
+
+
+public class LoginTest {
+	@Test
+void login() {
+	System.out.println("Login");
+}
+	
+}
